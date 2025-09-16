d66005ba3f7e3775b68f3bb94e37d679f4007d4a
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-09-16 22:59:42 +0530

    Add authentication with NextAuth and Prisma adapter, implement login and signup pages, and update dependencies

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { IconBrandGoogle, IconBrandGoogleFilled } from "@tabler/icons-react";
+import { Loader2 } from "lucide-react";
+
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <section className="bg-linear-to-b from-muted to-background flex min-h-screen px-4 py-16 md:py-32 justify-center items-center ">
+      <div>
+        <Card className="overflow-hidden p-0">
+          <CardContent className="grid p-0 md:grid-cols-2">
+            <form
+              onSubmit={() => {}}
+              action=""
+              className="max-w-92 m-auto h-fit w-full "
+            >
+              <div className="p-6">
+                <div>
+                  <img src="/logo/logo-light.svg" className="w-12" />
+                  <h1 className="mt-6 text-balance text-xl font-semibold">
+                    <span className="text-muted-foreground">
+                      Welcome back to Arc Labs!
+                    </span>{" "}
+                    Sign in to continue
+                  </h1>
+                </div>
+                <div className="mt-6 space-y-2">
+                  <Button className=" w-full flex cursor-pointer">
+                    <IconBrandGoogleFilled /> Google
+                  </Button>
+                </div>
+                <div className="flex mb-5 mt-6 items-center justify-center gap-4">
+                  <hr className="flex-grow border-t    border-dashed " />
+                  <span className="text-muted-foreground">
+                    or continue with
+                  </span>
+                  <hr className="flex-grow border-t  border-dashed " />
+                </div>
+
+                <div className="space-y-6">
+                  <div className="space-y-2">
+                    <Input
+                      type="email"
+                      required
+                      id="email"
+                      placeholder="Your email"
+                      className={`ring-foreground/15 border-transparent ring-1 `}
+                    />
+
+                    <Input
+                      type="password"
+                      required
+                      id="password"
+                      placeholder="Your password"
+                      className={`
+                      
+                       ring-foreground/15 border-transparent ring-1 `}
+                    />
+                  </div>
+
+                  <Button className="w-full cursor-pointer" size="default">
+                    Sign In
+                  </Button>
+                </div>
+              </div>
+
+              <div className="px-6 mb-5">
+                <p className="text-muted-foreground text-sm">
+                  Don't have an account ?
+                  <Button asChild variant="link" className="px-2">
+                    <Link href="/auth/sign-up">Create account</Link>
+                  </Button>
+                </p>
+              </div>
+            </form>
+            <div className="bg-muted relative hidden md:block">
+              <img
+                src="https://i.pinimg.com/736x/a7/70/cd/a770cd33fdd08ed309fb2ecf3a0af6d3.jpg"
+                alt="Image"
+                className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              />
+            </div>
+          </CardContent>
+        </Card>
+        <div className="text-muted-foreground mt-5 *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+          By clicking continue, you agree to our{" "}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
