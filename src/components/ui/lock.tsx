30c51170f0eb6a040195d4fbdd3faa66d76e32e4
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-09-25 17:54:38 +0530

    feat: simplify lock message in Lock component

--- src/components/ui/lock.tsx
+++ src/components/ui/lock.tsx
@@ -5,9 +5,7 @@ const Lock = () => {
   return (
     <div className=" absolute flex flex-col gap-3  bg-radial to-background   bg-white/40 backdrop-blur-md z-50 inset-0  justify-center items-center rounded-md">
       <LockIcon size={30} className=" mix-blend-difference" />
-      <p className="text-sm text-gray-500">
-        This project is locked. Please log in to unlock.
-      </p>
+      <p className="text-sm text-gray-500">This project is locked.</p>
     </div>
   );
 };
